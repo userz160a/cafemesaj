eacdb818e6702b21e3542af200545f0fb7263e16
userz160a <user.z160a@gmail.com>
2026-06-03 23:44:37 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.atelier801.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
